cf2c2f11853361cc5dd97680d85048a219877f2b
RadioAddition <radioaddition@pm.me>
2025-02-06 00:54:36 -0500

    Update .config/fish/config.fish Update .config/fish/fish_variables Update .config/rebos/gen.toml

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,26 +1,68 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-      # VI mode
-      fish_vi_key_bindings
-      set -Ux fish_cursor_default block
-      set -Ux fish_cursor_insert line
-      set -Ux fish_cursor_replace underscore
-      set -Ux fish_cursor_replace_one underscore
+    # Colors
+    set -Ux fish_initialized 3400
+    set -Ux fish_color_autosuggestion 666
+    set -Ux fish_color_cancel \x2d\x2dreverse
+    set -Ux fish_color_command 00ff00
+    set -Ux fish_color_comment 888\x1e\x2d\x2ditalics
+    set -Ux fish_color_cwd 0A0
+    set -Ux fish_color_cwd_root A00
+    set -Ux fish_color_end ffff00
+    set -Ux fish_color_error F22
+    set -Ux fish_color_escape 0AA
+    set -Ux fish_color_history_current 0AA
+    set -Ux fish_color_host normal
+    set -Ux fish_color_host_remote \x1d
+    set -Ux fish_color_keyword \x1d
+    set -Ux fish_color_match 0AA
+    set -Ux fish_color_normal B2B2B2
+    set -Ux fish_color_operator 0AA
+    set -Ux fish_color_option \x1d
+    set -Ux fish_color_param 0087d7
+    set -Ux fish_color_quote ffd75f
+    set -Ux fish_color_redirection 00ffd7
+    set -Ux fish_color_search_match \x2d\x2dbackground\x3d533
+    set -Ux fish_color_selection \x2d\x2dbackground\x3dB218B2
+    set -Ux fish_color_status red
+    set -Ux fish_color_user brgreen
+    set -Ux fish_color_valid_path \x2d\x2dunderline
+    set -Ux fish_key_bindings fish_default_key_bindings
+    set -Ux fish_pager_color_background \x1d
+    set -Ux fish_pager_color_completion BBB
+    set -Ux fish_pager_color_description 666
+    set -Ux fish_pager_color_prefix 0AA\x1e\x2d\x2dunderline
+    set -Ux fish_pager_color_progress 0AA
+    set -Ux fish_pager_color_secondary_background \x1d
+    set -Ux fish_pager_color_secondary_completion \x1d
+    set -Ux fish_pager_color_secondary_description \x1d
+    set -Ux fish_pager_color_secondary_prefix \x1d
+    set -Ux fish_pager_color_selected_background \x2d\x2dbackground\x3d333
+    set -Ux fish_pager_color_selected_completion \x1d
+    set -Ux fish_pager_color_selected_description \x1d
+    set -Ux fish_pager_color_selected_prefix \x1d
 
-      # shell inits
-      function starship_transient_prompt_func
-        starship module character
-      end
-      starship init fish | source
-      enable_transience
-      atuin init fish | source
-      direnv hook fish | source
-      chezmoi completion fish | source
-      just --completions fish | source
-      nh completions --shell fish | source
-      thefuck --alias | source
+    # VI mode
+    fish_vi_key_bindings
+    set -Ux fish_cursor_default block
+    set -Ux fish_cursor_insert line
+    set -Ux fish_cursor_replace underscore
+    set -Ux fish_cursor_replace_one underscore
 
-      # Misc variables
-      set -Ux HOSTNAME $(hostname)
-      set -Ux MANPAGER 'nvim +Man!'
+    # shell inits
+    function starship_transient_prompt_func
+      starship module character
+    end
+    starship init fish | source
+    enable_transience
+    atuin init fish | source
+    direnv hook fish | source
+    chezmoi completion fish | source
+    just --completions fish | source
+    nh completions --shell fish | source
+    thefuck --alias | source
+
+    # Misc variables
+    set -Ux HOSTNAME $(hostname)
+    set -Ux MANPAGER 'nvim +Man!'
 end
