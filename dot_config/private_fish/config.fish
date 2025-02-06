40c44469f45db7a759c74478f08f7c2787a39ce6
RadioAddition <radioaddition@pm.me>
2025-02-06 00:48:24 -0500

    Update .config/fish/config.fish Update .config/fish/fish_variables Update .config/rebos/gen.toml Update .config/rebos/managers/flatpak-verified.toml

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,3 +1,26 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+      # VI mode
+      fish_vi_key_bindings
+      set -Ux fish_cursor_default block
+      set -Ux fish_cursor_insert line
+      set -Ux fish_cursor_replace underscore
+      set -Ux fish_cursor_replace_one underscore
+
+      # shell inits
+      function starship_transient_prompt_func
+        starship module character
+      end
+      starship init fish | source
+      enable_transience
+      atuin init fish | source
+      direnv hook fish | source
+      chezmoi completion fish | source
+      just --completions fish | source
+      nh completions --shell fish | source
+      thefuck --alias | source
+
+      # Misc variables
+      set -Ux HOSTNAME $(hostname)
+      set -Ux MANPAGER 'nvim +Man!'
 end
