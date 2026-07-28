a7da845c87654e2f4bc078adffe3d66e393fef37
RadioAddition <radioaddition@pm.me>
2026-07-28 10:01:08 -0400

    lix install script improvements

--- .chezmoiscripts/run_once_lix.sh
+++ .chezmoiscripts/run_once_lix.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-which sudo && curl -sSf -L https://install.lix.systems/lix | sh -s -- install || run0 sh -c "curl -sSf -L https://install.lix.systems/lix | sh -s -- install"
+which sudo && export root="sudo" || root="run0 -i"
+curl -sSf -L https://install.lix.systems/lix | sh -s -- install || $root "curl -sSf -L https://install.lix.systems/lix | sh -s -- install"
